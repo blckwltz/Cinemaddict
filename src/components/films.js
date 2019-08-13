bac1522a32558703d3d19727677d5256b0cdeca2
blckwltz <ruslanbreev@gmail.com>
2019-08-13 23:14:37 +0300

    Добавляет отрисовку оставшихся карточек

--- src/components/films.js
+++
@@ -1,1 +0,0 @@
-export const createFilmsTemplate = () => `<section class="films"></section>`;
--- src/main.js
+++ src/main.js
@@ -1,33 +1,40 @@
+import {filmCard} from "./data";
 import {createSearchTemplate} from './components/search';
 import {createProfileRatingTemplate} from "./components/profile-rating";
 import {createMenuTemplate} from "./components/menu";
 import {createSortTemplate} from "./components/sort";
-import {createFilmsTemplate} from "./components/films";
 import {createFilmsListTemplate} from "./components/films-list";
 import {createFilmCardTemplate} from "./components/film-card";
 import {createShowMoreButtonTemplate} from "./components/show-more-button";
-import {createFilmDetailsTemplate} from "./components/film-details";
+// import {createFilmDetailsTemplate} from "./components/film-details";
+
+const GENERAL_FILMS_AMOUNT = 5;
+const EXTRA_FILMS_AMOUNT = 2;
 
 const headerElement = document.querySelector(`.header`);
 const mainElement = document.querySelector(`.main`);
-const createElementFromTemplate = (template) => {
-  let tempContainer = document.createElement(`div`);
-  tempContainer.innerHTML = template;
-  return tempContainer.firstChild;
+
+const renderElement = (element, template, position = `beforeend`, count = 0) => {
+  return count ? element.insertAdjacentHTML(position, new Array(count).fill(template).join(``)) : element.insertAdjacentHTML(position, template);
 };
-const renderElement = (element, template, position = `beforeend`) => {
-  element.insertAdjacentHTML(position, template);
-};
-const filmsElement = createElementFromTemplate(createFilmsTemplate());
-const filmsListElement = createElementFromTemplate(createFilmsListTemplate(`All movies. Upcoming`));
-const filmsContainerElement = filmsListElement.querySelector(`.films-list__container`);
 
 renderElement(headerElement, createSearchTemplate());
 renderElement(headerElement, createProfileRatingTemplate());
 renderElement(mainElement, createMenuTemplate());
 renderElement(mainElement, createSortTemplate());
-filmsElement.appendChild(filmsListElement);
-mainElement.appendChild(filmsElement);
-renderElement(filmsContainerElement, createFilmCardTemplate());
-renderElement(filmsListElement, createShowMoreButtonTemplate());
-renderElement(document.body, createFilmDetailsTemplate());
+renderElement(mainElement, createFilmsListTemplate());
+
+const mainFilmsListElement = document.querySelector(`.films-list`);
+const mainFilmsListContainerElement = mainFilmsListElement.querySelector(`.films-list__container`);
+
+renderElement(mainFilmsListElement, createShowMoreButtonTemplate());
+renderElement(mainFilmsListContainerElement, createFilmCardTemplate(), `beforeend`, GENERAL_FILMS_AMOUNT);
+
+const extraFilmsListElements = Array.from(document.querySelectorAll(`.films-list--extra`));
+
+extraFilmsListElements.forEach(function (filmsList) {
+  const container = filmsList.querySelector(`.films-list__container`);
+  renderElement(container, createFilmCardTemplate(), `beforeend`, EXTRA_FILMS_AMOUNT);
+});
+
+// renderElement(document.body, createFilmDetailsTemplate());
